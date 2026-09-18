a1198d0f68210ec04d74d7bb9af1d55be7c5e988
yifengyou <842056007@qq.com>
2026-09-18 11:02:30 +0800

    add build script
    Signed-off-by: yifengyou <842056007@qq.com>

---
+++ kdev-build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+# clean
+rm -rf output
+mkdir -p output
+
+# only spi
+rm -f uboot.img arch/arm/dts/rk3588-evb.dtb
+sed -i "s#model = .*#model = \"LZ D3588 Compiled By yifengyou v$(date +%Y.%m.%d-%H:%M:%S)\";#" arch/arm/dts/rk3588-evb.dts
+
+./make.sh rk3588
+
+dtc -I dtb -O dts arch/arm/dts/rk3588-evb.dtb -o lz_d3588-uboot.dts
+fdtdump u-boot.dtb >u-boot.dts
+ls -alh lz_d3588-uboot.dts u-boot.dts
+
+./make.sh loader
+ls -alh rk3588_spl*.bin
+mv rk3588_spl_loader_v1.15.113.bin output/rk3588_spl_loader_v1.15.113.bin
+
+dd if=uboot.img of=output/uboot.img bs=2M count=1
+dumpimage -l output/uboot.img
+ls -alh output/uboot.img
+
+# show output
+ls -alh output/*
+sha256sum output/*
+echo "All ok! All done!"
---
+++ kdev-menuconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+make CROSS_COMPILE=`pwd`/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- \
+	menuconfig
+make CROSS_COMPILE=`pwd`/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- \
+	savedefconfig
